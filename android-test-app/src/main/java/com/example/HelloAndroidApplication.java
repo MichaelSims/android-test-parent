11197a0f1fae72e20e952cabbd12ccb10cc7f0d6
Michael Sims <msims@mercuryintermedia.com>
2013-11-06 11:10:05 -0600

    add application class

---
+++ android-test-app/src/main/java/com/example/HelloAndroidApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import android.app.Application;
+
+public class HelloAndroidApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
