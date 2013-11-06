56ea522bb48fa34a1a1d3a174f6b061ecd4b91cd
Michael Sims <msims@mercuryintermedia.com>
2013-11-06 11:07:11 -0600

    cleanups from template

--- android-test-app/src/main/java/com/example/HelloAndroidActivity.java
+++ android-test-app/src/main/java/com/example/HelloAndroidActivity.java
@@ -2,17 +2,10 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 
 public class HelloAndroidActivity extends Activity {
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +14,9 @@ public class HelloAndroidActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.example.R.menu.main, menu);
-	return true;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.example.R.menu.main, menu);
+        return true;
     }
 
 }
