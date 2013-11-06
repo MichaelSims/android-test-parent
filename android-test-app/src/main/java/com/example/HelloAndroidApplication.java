7a322e7dafe883aa4542165119dcdd6539293e15
Michael Sims <msims@mercuryintermedia.com>
2013-11-06 11:13:33 -0600

    Log all messages on the event queue

--- android-test-app/src/main/java/com/example/HelloAndroidApplication.java
+++ android-test-app/src/main/java/com/example/HelloAndroidApplication.java
@@ -1,10 +1,20 @@
 package com.example;
 
 import android.app.Application;
+import android.util.Log;
+import android.util.Printer;
 
 public class HelloAndroidApplication extends Application {
+    private final static String TAG = HelloAndroidApplication.class.getSimpleName();
+
     @Override
     public void onCreate() {
         super.onCreate();
+        getMainLooper().setMessageLogging(new Printer() {
+            @Override
+            public void println(final String x) {
+                Log.e(TAG, ">>> " + x);
+            }
+        });
     }
 }
